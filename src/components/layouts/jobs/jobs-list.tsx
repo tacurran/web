33019e8cc38f96239d1db32454b5013fee7abeab
jfcurran <john@ory.sh>
2021-07-07 17:50:39 +0200

    change appearance of jobs-list, WIP
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/jobs/jobs-list.tsx
+++ src/components/layouts/jobs/jobs-list.tsx
@@ -80,61 +80,57 @@ const JobsList = ({ title, description, buttons }: PropTypes) => {
     }
   `)
   return (
-    <div className={cn(styles.jobList)}>
-      <Container fluid={true} alignItems={'start'}>
-        <Grid lg={4} md={3} sm={12} xs={12} className={cn(pb64)}>
-          <ContentText>
-            <Molecule>
-              <h2 className={cn('font-h3')}>{title}</h2>
-              <p className={cn('font-p', pt32)}>{description}</p>
-            </Molecule>
-            <MoleculeInteraction className={cn(pt24)}>
-              {buttons}
-            </MoleculeInteraction>
-          </ContentText>
-        </Grid>
-        <Grid lg={6} md={8} sm={12} xs={12}>
-          <Container
-            alignItems={'start'}
-            justify={'start'}
-            className={cn(styles.jobs)}
-          >
-            {(data.allMdx.edges as Edge[]).map(({ node }) => (
-              <Button
-                style={'outlined'}
-                to={node.frontmatter.path}
-                className={cn(styles.jobItem)}
-                key={node.id}
-              >
-                <div className={cn(styles.jobInfo, pb16)}>
-                  <h3 className={cn(styles.jobTitle, 'font-h5', pb8)}>
-                    {node.frontmatter.title}
-                  </h3>
+    <Container fluid={true} alignItems={'start'} className={cn(styles.jobList)}>
+      <Grid lg={4} md={3} sm={12} xs={12} className={cn(pb64)}>
+        <ContentText>
+          <Molecule>
+            <h2 className={cn('font-h3')}>{title}</h2>
+            <p className={cn('font-p', pt32)}>{description}</p>
+          </Molecule>
+          <MoleculeInteraction className={cn(pt24)}>
+            {buttons}
+          </MoleculeInteraction>
+        </ContentText>
+      </Grid>
+      <Grid lg={6} md={8} sm={12} xs={12}>
+        <Container
+          alignItems={'start'}
+          justify={'start'}
+          className={cn(styles.jobs)}
+        >
+          {(data.allMdx.edges as Edge[]).map(({ node }) => (
+            <Button
+              style={'outlined'}
+              to={node.frontmatter.path}
+              className={cn(styles.jobItem)}
+              key={node.id}
+            >
+              <div className={cn(styles.jobInfo, pb16)}>
+                <h3 className={cn(styles.jobTitle, 'font-h5', pb8)}>
+                  {node.frontmatter.title}
+                </h3>
 
-                  <div className={cn(styles.jobDetails)}>
-                    <MapPinLine />
-                    <p className={cn('font-p-xs', pl8)}>
-                      <b>{node.frontmatter.location}</b>
-                    </p>
-                  </div>
-                  <div className={cn(styles.jobDetails)}>
-                    <ClockClockwise />
-                    <p className={cn('font-p-xs', pl8)}>
-                      {node.frontmatter.position}
-                    </p>
-                  </div>
+                <div className={cn(styles.jobDetails)}>
+                  <MapPinLine />
+                  <p className={cn('font-p-xs', pl8)}>
+                    <b>{node.frontmatter.location}</b>
+                  </p>
                 </div>
-                <p
-                  className={cn(styles.jobDescription, 'font-p-sm', 'mute-85')}
-                >
-                  {node.frontmatter.description} <b>Learn more</b>
-                </p>
-              </Button>
-            ))}
-          </Container>
-        </Grid>
-      </Container>
-    </div>
+                <div className={cn(styles.jobDetails)}>
+                  <ClockClockwise />
+                  <p className={cn('font-p-xs', pl8)}>
+                    {node.frontmatter.position}
+                  </p>
+                </div>
+              </div>
+              <p className={cn(styles.jobDescription, 'font-p-sm', 'mute-85')}>
+                {node.frontmatter.description} <b>Learn more</b>
+              </p>
+            </Button>
+          ))}
+        </Container>
+      </Grid>
+    </Container>
   )
 }
 
