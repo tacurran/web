1a0970bff34b11c47423bcc0638242fa5f88ab2b
jfcurran <john@ory.sh>
2021-07-09 14:37:16 +0200

    add optional bool prop 'thin' to make component thinner on pages like jobs.tsx
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/hero/hero-features.tsx
+++ src/components/layouts/hero/hero-features.tsx
@@ -19,63 +19,127 @@ export interface Text {
 }
 
 interface PropTypes {
+  thin?: boolean
   small?: boolean
   title: string
   description?: React.ReactElement
   textbox: Array<Text>
 }
 
-const HeroFeatures = ({ small, title, description, textbox }: PropTypes) => (
+const HeroFeatures = ({
+  thin,
+  small,
+  title,
+  description,
+  textbox
+}: PropTypes) => (
   <div className={cn(styles.heroFeatures)}>
     <Container fluid={true} justify={'center'} alignItems={'start'}>
-      <Grid lg={12} md={12} sm={12} xs={12}>
-        <Container fluid={true} justify={'center'} className={cn(pb64)}>
-          <Grid
-            lg={6}
-            md={12}
-            sm={12}
-            xs={12}
-            className={cn(styles.heroFeaturesHeading)}
-          >
-            {!small ? (
-              <h1 className={cn('font-h1', pb32)}>{title}</h1>
-            ) : (
-              <h2 className={cn('font-h3', pb32)}>{title}</h2>
-            )}
-            {description && (
-              <p className={cn('font-p-lg', 'mute-85', pb8)}>{description}</p>
-            )}
-          </Grid>
-        </Container>
-        <Container alignItems={'start'} justify={'start'}>
-          {textbox.map((f, index) => {
-            return (
-              <Grid
-                lg={6}
-                md={6}
-                sm={6}
-                xs={12}
-                className={cn(pb48, styles.heroFeaturesContent)}
-                key={index}
-              >
-                <Container flexContainer={'row'} alignItems={'start'}>
-                  {f.icon && (
-                    <ColourWrapper className={cn(pb8)} text={'themed-primary'}>
-                      {f.icon}
-                    </ColourWrapper>
-                  )}
-                  <ContentText>
-                    <h4 className={cn('font-h5', pb8)}>{f.title}</h4>
-                    <p className={cn('font-p-sm', 'mute-60', pb8)}>
-                      {f.description}
-                    </p>
-                  </ContentText>
-                </Container>
-              </Grid>
-            )
-          })}
-        </Container>
-      </Grid>
+      {!thin ? (
+        <Grid lg={12} md={12} sm={12} xs={12}>
+          <Container justify={'center'} className={cn(pb64)}>
+            <Grid
+              lg={8}
+              md={10}
+              sm={12}
+              xs={12}
+              className={cn(styles.heroFeaturesHeading)}
+            >
+              {!small ? (
+                <h1 className={cn('font-h1', pb32)}>{title}</h1>
+              ) : (
+                <h2 className={cn('font-h3', pb32)}>{title}</h2>
+              )}
+              {description && (
+                <p className={cn('font-p-lg', 'mute-85', pb8)}>{description}</p>
+              )}
+            </Grid>
+          </Container>
+          <Container alignItems={'start'} justify={'start'}>
+            {textbox.map((f, index) => {
+              return (
+                <Grid
+                  lg={6}
+                  md={6}
+                  sm={6}
+                  xs={12}
+                  className={cn(pb48, styles.heroFeaturesContent)}
+                  key={index}
+                >
+                  <Container flexContainer={'row'} alignItems={'start'}>
+                    {f.icon && (
+                      <ColourWrapper
+                        className={cn(pb8)}
+                        text={'themed-primary'}
+                      >
+                        {f.icon}
+                      </ColourWrapper>
+                    )}
+                    <ContentText>
+                      <h4 className={cn('font-h5', pb8)}>{f.title}</h4>
+                      <p className={cn('font-p-sm', 'mute-60', pb8)}>
+                        {f.description}
+                      </p>
+                    </ContentText>
+                  </Container>
+                </Grid>
+              )
+            })}
+          </Container>
+        </Grid>
+      ) : (
+        <Grid lg={8} md={10} sm={12} xs={12}>
+          <Container fluid={true} justify={'center'} className={cn(pb64)}>
+            <Grid
+              lg={8}
+              md={10}
+              sm={12}
+              xs={12}
+              className={cn(styles.heroFeaturesHeading)}
+            >
+              {!small ? (
+                <h1 className={cn('font-h1', pb32)}>{title}</h1>
+              ) : (
+                <h2 className={cn('font-h3', pb32)}>{title}</h2>
+              )}
+              {description && (
+                <p className={cn('font-p-lg', 'mute-85', pb8)}>{description}</p>
+              )}
+            </Grid>
+          </Container>
+          <Container alignItems={'start'} justify={'start'}>
+            {textbox.map((f, index) => {
+              return (
+                <Grid
+                  lg={6}
+                  md={6}
+                  sm={6}
+                  xs={12}
+                  className={cn(pb48, styles.heroFeaturesContent)}
+                  key={index}
+                >
+                  <Container flexContainer={'row'} alignItems={'start'}>
+                    {f.icon && (
+                      <ColourWrapper
+                        className={cn(pb8)}
+                        text={'themed-primary'}
+                      >
+                        {f.icon}
+                      </ColourWrapper>
+                    )}
+                    <ContentText>
+                      <h4 className={cn('font-h5', pb8)}>{f.title}</h4>
+                      <p className={cn('font-p-sm', 'mute-60', pb8)}>
+                        {f.description}
+                      </p>
+                    </ContentText>
+                  </Container>
+                </Grid>
+              )
+            })}
+          </Container>
+        </Grid>
+      )}
     </Container>
   </div>
 )
