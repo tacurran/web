d4071b68aa20887ae1443c5db9ef6c9d2d19dcd5
jfcurran <john@ory.sh>
2021-07-13 15:26:48 +0200

    update profile to make use of new StaticImage from gatsby v3
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/profile.tsx
+++ src/components/profile.tsx
@@ -1,9 +1,8 @@
 import cn from 'classnames'
-import { GatsbyImage } from 'gatsby-plugin-image'
 import React from 'react'
 
 import Button from './freestanding/button/button'
-import IconWrapper from './freestanding/icon/icon-wrapper'
+import ColourWrapper from './freestanding/colour/colour-wrapper'
 
 import { pb16, pb8, pr16, pr8 } from './freestanding/utils/padding.module.css'
 import * as styles from './profile.module.css'
@@ -64,14 +63,12 @@ const socialWithIcon = ({ href, network }: SocialLinks): resolvedSocial => {
 
 const Profile = ({ name, img, social }: PropTypes) => (
   <div className={cn(styles.profile, pb16)}>
-    <div className={cn(pr16)}>
-      <GatsbyImage image={img} alt={name} />
-    </div>
+    <div className={cn(pr16)}>{img}</div>
     <div>
       <h2 className={cn('font-h5', pb8)}>{name}</h2>
       <>
         {social.map(socialWithIcon).map(({ Icon, href }) => (
-          <Button to={href} style={'none'} key={href} className={cn(pr8)}>
+          <Button key={href} to={href} style={'none'} className={pr8}>
             {Icon}
           </Button>
         ))}
