b3c196d5cd98e3f3f32637e80f28c12214d91751
jfcurran <john@ory.sh>
2021-07-16 00:54:26 +0200

    create simple and small text components for use in team and team-image and about
    Signed-off-by: jfcurran <john@ory.sh>

--- src/components/layouts/simple-text/simple-text.tsx
+++ src/components/layouts/simple-text/simple-text.tsx
@@ -1,5 +1,5 @@
 import cn from 'classnames'
-import React from 'react'
+import React, { ReactElement } from 'react'
 
 import Container from '../../freestanding/containers/container'
 import Grid from '../../freestanding/containers/grid'
@@ -11,21 +11,22 @@ interface PropTypes {
   title: string
   small?: boolean
   text: React.ReactElement
+  className?: string
 }
 
-const SimpleText = ({ title, small, text }: PropTypes) => (
-  <div className={cn(styles.simpleText)}>
+const SimpleText = ({ title, small, text, className }: PropTypes) => (
+  <div className={cn(styles.simpleText, `${className}`)}>
     <Container fluid={true} alignItems={'start'} justify={'center'}>
       <Grid lg={8} md={10} sm={12} xs={12}>
         <Container alignItems={'start'}>
-          <Grid lg={5} md={6} sm={12} xs={12}>
+          <Grid lg={4} md={4} sm={12} xs={12}>
             {!small ? (
               <h1 className={cn('font-h1', pb32)}>{title}</h1>
             ) : (
               <h3 className={cn('font-h3', pb32)}>{title}</h3>
             )}
           </Grid>
-          <Grid lg={6} md={6} sm={12} xs={12}>
+          <Grid lg={8} md={8} sm={12} xs={12}>
             <p className={cn('font-p-lg', 'mute-85')}>{text}</p>
           </Grid>
         </Container>
