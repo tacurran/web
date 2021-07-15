f7eb889bd1da4d8b77573d8ad5423f1a943df1b9
jfcurran <john@ory.sh>
2021-07-15 14:58:15 +0200

    use simple-text.tsx on about.tsx and add team.tsx with teamMembers
    Signed-off-by: jfcurran <john@ory.sh>

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 import Layout from '../components/layouts/layout/layout'
 import SEO from '../components/layouts/seo/seo'
-import Team from '../components/team'
+import SimpleText from '../components/layouts/simple-text/simple-text'
+import Team from '../components/layouts/team/team'
+import { teamMembers } from '../components/layouts/team/team-members'
 
 const AboutPage = () => (
   <Layout>
@@ -10,7 +12,22 @@ const AboutPage = () => (
       description="Secure open source IAM and access control infrastructure for the cloud."
       title="Ory - A modern open source identity solution"
     />
-    <Team />
+    <SimpleText
+      title={'About Us'}
+      text={
+        <>
+          Our mission is to provide a common identity and access infrastructure
+          that manages how IAM data is used in cloud applications. We deliver
+          the data and associated analytics to members in the network in
+          accordance with data privacy and usage conventions. We embrace and
+          practice open source software development. We base our work on
+          existing and emerging open cloud standards. As developers, we will
+          continue to push the boundaries in engineering and design to help
+          other developers succeed.
+        </>
+      }
+    />
+    <Team title={'The team'} team={teamMembers} />
   </Layout>
 )
 
